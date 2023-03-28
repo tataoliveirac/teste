672e7b61058356393f94242258e82d83bc7508db
tataoliveirac <122825262+tataoliveirac@users.noreply.github.com>
2023-03-28 13:44:32 -0300

    adicionando arquivo inicial- teste

---
+++ desafio.js
@@ -0,0 +1,28 @@
+function listaCompras(itens, item) {
+    let lista = ["banana", "abacaxi"]
+    switch (itens) {
+        case `adicionar`:
+            console.log(lista.push);
+            lista.push(item)
+            break;
+        case `remover`:
+            console.log(lista.pop);
+            removed = lista.pop()
+            break;
+        case `remover por posicao`:
+            console.log(lista.splice);
+            removed = lista.splice(1,1)
+            break;
+
+        default:
+            console.log("erro");
+            break;
+
+    }
+    return lista;
+}
+console.log(listaCompras('remover', 'melancia'))
+
+
+
+
